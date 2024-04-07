258314158265e51e43f38fe2b362f75d3fab3e98
Luka Gomzi <gomzi.luka77@gmail.com>
2024-04-07 18:06:17 +0200

    fix: Handled get user id decorator better

--- src/core/decorators/get-user-id.decorator.ts
+++ src/core/decorators/get-user-id.decorator.ts
@@ -1,8 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetUserId = createParamDecorator(
-    (data: unknown, ctx: ExecutionContext): number => {
+    (data: unknown, ctx: ExecutionContext): number | null => {
         const request = ctx.switchToHttp().getRequest();
-        return request.user.userId;
+        if (request.user?.userId) return request.user?.userId;
+        else return null;
     },
 );
