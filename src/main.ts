d44f1503f5adc30b25e82a95523e0c72ad69610a
Luka Gomzi <gomzi.luka77@gmail.com>
2024-05-19 23:38:31 +0200

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
 
   await app.listen(3000);
 }
