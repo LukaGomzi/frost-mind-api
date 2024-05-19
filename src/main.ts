b79fd09689b468bb9ca405f10f46fb692dd4d617
Luka Gomzi <gomzi.luka77@gmail.com>
2024-05-19 23:36:22 +0200

    another cors try

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.use(cors({
-    origin: true,
+  app.enableCors({
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-  }));
+  });
+
   await app.listen(3000);
 }
 bootstrap();
