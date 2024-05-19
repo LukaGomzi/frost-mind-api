1ffa6373119e1b953340612eebdd8b1e6f9b0938
Luka Gomzi <gomzi.luka77@gmail.com>
2024-05-19 23:45:52 +0200

    New deploy

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set global prefix
   app.setGlobalPrefix('api/v1');
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  // });
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
