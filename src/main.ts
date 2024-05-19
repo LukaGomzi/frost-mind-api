3be674458a2f6c100908f1a9611e6007f9e9f26d
Luka Gomzi <gomzi.luka77@gmail.com>
2024-05-19 23:28:30 +0200

    applied some cors settings

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api/v1');
+  app.use(cors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
